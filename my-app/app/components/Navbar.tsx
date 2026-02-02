053ac0da84b0a460b2d606d52de39635f2aa2e65
Imam mka <imammka23@gmail.com>
2026-02-02 15:11:01 +0700

    progress navbar

--- my-app/app/components/Navbar.tsx
+++ my-app/app/components/Navbar.tsx
@@ -1,9 +1,22 @@
-
-
 export default function NavBar() {
-    return (
-    <nav>
-      <h1>Kodein Edu Center</h1>
-    </nav>
-);
+  return (
+    <header className="bg-white shadow text-black border-1 border-black">
+      <nav className="mx-auto flex flex-row justify-between items-center">
+          <div>
+            <a href="#">
+              <img src="logoKodeinEdu.png" alt="" className="w-[168px] h-[63px] object-contain "/>
+            </a>
+          </div>
+
+          <div>
+            <ul className="flex flex-row gap-4">
+              <li>Home</li>
+              <li>About</li>
+              <li>Services</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+      </nav>
+    </header>
+  );
 }
