5bc1d298a9c1bfa63f4df459eac1ab19e2a4b3ad
Zahidalwan <zahidalwan1601@gmail.com>
2026-02-04 13:53:57 +0700

    update

--- my-app/app/components/Navbar.tsx
+++ my-app/app/components/Navbar.tsx
@@ -1,21 +1,25 @@
 export default function NavBar() {
   return (
-    <header className="bg-white shadow text-black border-1 border-black">
-      <nav className="mx-auto flex flex-row justify-between items-center">
-          <div>
-            <a href="#">
-              <img src="logoKodeinEdu.png" alt="" className="w-[168px] h-[63px] object-contain "/>
-            </a>
-          </div>
+    <header className="bg-white shadow-xl text-black m-7 rounded-2xl">
+      <nav className="mx-7 flex flex-row justify-between items-center">
+        <div>
+          <a href="#">
+            <img
+              src="logoKodeinEdu.png"
+              alt=""
+              className="w-42 h-15.75 object-contain "
+            />
+          </a>
+        </div>
 
-          <div>
-            <ul className="flex flex-row gap-4">
-              <li>Home</li>
-              <li>About</li>
-              <li>Services</li>
-              <li>Contact</li>
-            </ul>
-          </div>
+        <div>
+          <ul className="flex flex-row gap-4">
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
